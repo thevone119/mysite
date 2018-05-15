0544d029fd7657474fbf6dd33f102d7e0c4361d1
thevone119 <57717235@qq.com>
2018-05-16 03:33:15 +0800

    123

--- mysite/app/ipproxy/script/init_ip_proxy.py
+++ mysite/app/ipproxy/script/init_ip_proxy.py
@@ -2,9 +2,10 @@
 import os
 # 导入Django
 import django
-
+import requests
 
 def doAction(self, id):
+
     pass
 
     return
@@ -19,3 +20,10 @@ if __name__ == '__main__':
     # 这个导入不能写在头部,要先执行django进行一些环境初始化工作,否则无法初始化
     from mysite.app.ipproxy import models
 
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    }
+    r = requests.get('https://www.baidu.com/',headers=headers)
+    r.enconding = "utf-8"
+    print(r.text)
