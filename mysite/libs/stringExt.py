9307446803a651cd2792a9cc768e8bc290004dc5
thevone119 <57717235@qq.com>
2018-05-18 01:54:41 +0800

    123

---
+++ mysite/libs/stringExt.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#抽取字符串
+def ExtStr(src,star,end):
+    if src == None:
+        return None
+    idxs = src.find(star)
+    idxe = src.find(end)
+    if idxs==-1:
+        idxs = 0
+    else:
+        idxs=idxs+len(star)
+    if idxe==-1:
+        idxe=len(src)
+    return src[idxs:idxe]
+
+
+
