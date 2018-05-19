74dfe3ea0ed82904e59fa5907b9d5e754b6e4356
thevone119 <57717235@qq.com>
2018-05-20 03:28:28 +0800

    123

--- mysite/libs/stringExt.py
+++ mysite/libs/stringExt.py
@@ -1,18 +1,42 @@
 # -*- coding: utf-8 -*-
 
 #抽取字符串
-def ExtStr(src,star,end):
+def ExtStr(src,star,end=None):
     if src == None:
         return None
     idxs = src.find(star)
-    idxe = src.find(end)
     if idxs==-1:
         return None
     else:
         idxs=idxs+len(star)
+    if end is None:
+        return src[idxs:]
+    idxe = src.find(end)
     if idxe==-1:
         idxe=len(src)
     return src[idxs:idxe]
 
+#抽取字符串
+def extractLine(src,*args):
+    if src == None:
+        return None
+    sline = src.split("\n")
+    for l in sline:
+        l = l.strip()
+        if len(l) < 1:
+            continue
+        isf = True
+        for value in args:
+            if l.find(value) < 0:
+                isf = False
+                break
+        if isf:
+            return l
+    pass
+    return None
 
 
+if __name__ == '__main__':
+    st = "123456\n" \
+         "fasljdflasdj"
+    print(extractLine(st,"fas",'lj'))
