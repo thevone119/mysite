b033fa11faca740e9ac17801e45055314753701a
thevone119 <57717235@qq.com>
2018-05-21 02:21:50 +0800

    123

--- mysite/app/taobao/tbHttp.py
+++ mysite/app/taobao/tbHttp.py
@@ -12,7 +12,12 @@ import urllib
 
 from mysite.libs import BaseHttpGet
 
-#所有淘宝的httpget请求处理定义在这里
+# 所有淘宝的httpget请求处理定义在这里
+
+CRA_COUNT = 0
+# 全局锁
+L_CAT = threading.Lock()
+
 
 # 淘宝的商店搜索
 # https://shopsearch.taobao.com/search?app=shopsearch&q=%E5%A5%B3%E8%A3%85&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20180405&ie=utf8&loc=%E8%8C%82%E5%90%8D&s=540
@@ -25,6 +30,10 @@ class TBShopSearchCrawer(BaseHttpGet.BaseHttpGet):
 
     # 执行数据爬取前先设置headers
     def before(self):
+        global CRA_COUNT
+        L_CAT.acquire()
+        CRA_COUNT = CRA_COUNT + 1
+        L_CAT.release()
         isg = tbpool.popISG()
         tbpool.pushISG(isg)
         self.headers = {
@@ -33,9 +42,9 @@ class TBShopSearchCrawer(BaseHttpGet.BaseHttpGet):
         }
         now = time.strftime("%Y%m%d", time.localtime())
 
-        q = urllib.parse.urlencode({'q':self.q,'loc':self.city})
+        q = urllib.parse.urlencode({'q': self.q, 'loc': self.city})
         turl = "https://shopsearch.taobao.com/search?app=shopsearch&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_{0}&ie=utf8&s={1}&{2}"
-        self.url = turl.format(now,  self.pageno,q)
+        self.url = turl.format(now, self.pageno, q)
         return True
 
     # 这个方法由子类实现，爬虫爬取完成后，会调用此方法
@@ -46,22 +55,23 @@ class TBShopSearchCrawer(BaseHttpGet.BaseHttpGet):
             rettext = response.text
             # 成功的话，必然包含下面的字符串
             if rettext.find("g_page_config =") == -1:
-                print("数据抓取错误：" ,rettext)
+                print("数据抓取错误：", rettext, CRA_COUNT)
                 return False
             g_pagestr = stringExt.extractLine(rettext, "g_page_config", "pageName")
 
             if g_pagestr is None:
-
                 return False
             g_pagestr = stringExt.ExtStr(g_pagestr, "g_page_config = ")
-            g_pagestr = g_pagestr[:len(g_pagestr)-1]
+            g_pagestr = g_pagestr[:len(g_pagestr) - 1]
             # 如果没有shopItems，则抓取结束了
             if g_pagestr.find("shopItems") == -1:
                 myredis.set("TB:ShopSearch:" + self.q + "," + self.city, None, ex=60 * 60 * 6)
                 return True
             page = json.loads(g_pagestr)
             items = page["mods"]["shoplist"]["data"]["shopItems"]
+            itemcount = 0
             for item in items:
+                itemcount = itemcount + 1
                 shop = models.TTbShop()
                 shopurl = item["shopUrl"]
                 shop.shopid = self.paseInt(shopurl[shopurl.find("shop") + 4:shopurl.find(".taobao")])
@@ -70,7 +80,7 @@ class TBShopSearchCrawer(BaseHttpGet.BaseHttpGet):
                 shop.nick = item["nick"]
                 shop.user_rate_url = item['userRateUrl']
                 shop.title = item['title']
-                #shop.shop_score = self.paseInt(item['totalsold'])
+                # shop.shop_score = self.paseInt(item['totalsold'])
                 shop.prod_count = self.paseInt(item['procnt'])
                 shop.shop_area = item['provcity']
                 if item["isTmall"] is True:
@@ -78,20 +88,20 @@ class TBShopSearchCrawer(BaseHttpGet.BaseHttpGet):
                 else:
                     shop.shop_type = "TB"
 
-                #如果已经存在，则不处理，否则存储
+                # 如果已经存在，则不处理，否则存储
                 if tbpool.existKey(shop.shopid):
                     continue
                 else:
                     shop.save()
             pass
-            print("数据抓取结束",self.url)
+            print("数据抓取结束", self.city, self.q, self.pageno, L_CAT, itemcount)
             # 执行完，把一下页放入待执行列表
             if self.pageno < 100:
                 self.pageno = self.pageno + 1
                 self.id = None  # id必须设置为空，否则无放入到运行队列里
                 BaseHttpGet.pushHttpGet(self)
         except Exception as e:
-            print("TBShopSearchCrawer数据解析出错:",e)
+            print("TBShopSearchCrawer数据解析出错:", e)
 
         return True
         pass
@@ -109,9 +119,8 @@ class TestHttpGet(BaseHttpGet.BaseHttpGet):
         return True
 
 
-
 if __name__ == '__main__':
-    print(urllib.parse.urlencode({"q":"吃"}))
+    print(urllib.parse.urlencode({"q": "吃"}))
     test = TBShopSearchCrawer()
     test.city = "广州"
     test.q = "吃"
